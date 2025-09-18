f1aea3b12c59e8980222c57ec0b42c716fb7f0ef
Jainesh Kothari <63280686+jakoth@users.noreply.github.com>
2025-09-17 22:25:34 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,6 @@ export default function Home() {
     <div> 
       <iframe
         src="https://chatbase.co/TNjB_E4fqCEvhZ1jNSKwE/help"
-       // width="100%"
-       // height="800"
         style={{ border: "none" }}
         title="Chatbase Chatbot"
       ></iframe>
