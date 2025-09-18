7eec0675e7cf5f6c009e974a38f21b7a0cb0b3de
Jainesh Kothari <63280686+jakoth@users.noreply.github.com>
2025-09-17 22:11:17 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,14 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app yaar!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+    <div style={{ display: "flex", justifyContent: "center", padding: "2rem" }}>
+      <iframe
+        src="https://chatbase.co/TNjB_E4fqCEvhZ1jNSKwE/help"
+        width="100%"
+        height="800"
+        style={{ border: "none" }}
+        title="Chatbase Chatbot"
+      ></iframe>
     </div>
-  )
+  );
 }
