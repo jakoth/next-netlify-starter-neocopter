022117022e6d9c815608744ee2e2e939accb63e3
Jainesh Kothari <63280686+jakoth@users.noreply.github.com>
2025-09-17 22:20:44 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,15 @@
-// export default function Home() {
-//   return (
-//     <div style={{ display: "flex", justifyContent: "center", padding: "2rem" }}>
-//       <iframe
-//         src="https://chatbase.co/TNjB_E4fqCEvhZ1jNSKwE/help"
-//         width="100%"
-//         height="800"
-//         style={{ border: "none" }}
-//         title="Chatbase Chatbot"
-//       ></iframe>
-//     </div>
-//   );
-// }
-// next.config.js
+export default function Home() {
+  return (
+    <div> //style={{ display: "flex", justifyContent: "center", padding: "2rem" }}>
+      <iframe
+        src="https://chatbase.co/TNjB_E4fqCEvhZ1jNSKwE/help"
+       // width="100%"
+       // height="800"
+        style={{ border: "none" }}
+        title="Chatbase Chatbot"
+      ></iframe>
+    </div>
+  );
+}
+next.config.js
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/help",
-        destination: "https://chatbase.co/{agentId}/help",
-      },
-      // This second rule is needed to correctly proxy asset requests
-      // like CSS, JS, and images from your help center.
-      {
-        source: "/help/:path*",
-        destination: "https://chatbase.co/{agentId}/help/:path*",
-      },
-    ];
-  },
-};
-
-module.exports = nextConfig;
