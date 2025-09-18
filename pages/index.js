5b739ce8761f9e602923bb437aaaa60692d3aa18
Jainesh Kothari <63280686+jakoth@users.noreply.github.com>
2025-09-17 22:23:12 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
-    <div> //style={{ display: "flex", justifyContent: "center", padding: "2rem" }}>
+    <div> 
+    //style={{ display: "flex", justifyContent: "center", padding: "2rem" }}>
       <iframe
         src="https://chatbase.co/TNjB_E4fqCEvhZ1jNSKwE/help"
        // width="100%"
