e92aff3cad84265325b610e3d95aafbcda0dea2e
Jainesh Kothari <63280686+jakoth@users.noreply.github.com>
2025-09-17 22:36:25 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,32 @@
+// export default function Home() {
+//   return (
+//     <div> 
+//       <iframe
+//         src="https://chatbase.co/TNjB_E4fqCEvhZ1jNSKwE/help"
+//       ></iframe>
+//     </div>
+//   );
+// }
+// next.config.js
+
+import Script from "next/script";
+
 export default function Home() {
   return (
-    <div> 
-      <iframe
-        src="https://chatbase.co/TNjB_E4fqCEvhZ1jNSKwE/help"
-      ></iframe>
-    </div>
+    <>
+      <h1>Welcome to my site</h1>
+      <Script
+        src="https://www.chatbase.co/embed.min.js"
+        strategy="afterInteractive"
+        onLoad={() => {
+          window.Chatbase = {
+            config: {
+              chatbotId: "TNjB_E4fqCEvhZ1jNSKwE", // replace with your ID
+            },
+          };
+        }}
+      />
+      <div id="chatbase-root"></div>
+    </>
   );
 }
-next.config.js
-
